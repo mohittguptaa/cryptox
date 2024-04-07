986434b1196e1c430014e1e7b7d9efc098309c88
mohittguptaa <mohitgupta8218@gmail.com>
2024-04-07 21:26:04 +0530

    added authintication successfully

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,47 @@
 import { Button, HStack } from "@chakra-ui/react";
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Header = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
+
   return (
     <HStack p={"4"} shadow={"base"} bgColor={"blackAlpha.900"}>
       <Button variant={"unstyled"} color={"white"}>
         <Link to="/">Home</Link>
       </Button>
-      <Button variant={"unstyled"} color={"white"}>
-        <Link to="/exchanges">Exchanges</Link>
-      </Button>
-      <Button variant={"unstyled"} color={"white"}>
-        <Link to="/coins">Coins</Link>
-      </Button>
+
+      {isAuthenticated ? (
+        <>
+          <Button variant={"unstyled"} color={"white"}>
+            <Link to="/exchanges">Exchanges</Link>
+          </Button>
+          <Button variant={"unstyled"} color={"white"}>
+            <Link to="/coins">Coins</Link>
+          </Button>
+        </>
+      ) : null}
+
+      {isAuthenticated ? (
+        <Button
+          pl={1170}
+          variant={"unstyled"}
+          color={"white"}
+          onClick={() => logout()}
+        >
+          LogOut
+        </Button>
+      ) : (
+        <Button
+          pl={1300}
+          variant={"unstyled"}
+          color={"white"}
+          onClick={() => loginWithRedirect()}
+        >
+          Log In
+        </Button>
+      )}
     </HStack>
   );
 };
--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ChakraProvider, theme } from "@chakra-ui/react";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-      <App />
+      <Auth0Provider
+        domain="dev-pfdacm5etiyckirm.us.auth0.com"
+        clientId="IyeUaud5jz9UrT6Pq8uTXcgOSkNTFxTo"
+        authorizationParams={{
+          redirect_uri: window.location.origin,
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </ChakraProvider>
   </React.StrictMode>
 );
